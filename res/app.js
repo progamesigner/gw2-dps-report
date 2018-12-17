ba8b1a17c09645a5e318bde2b90111964379fe1e
Yang Sheng Han <progamesigner@gmail.com>
2018-12-17 21:17:50 +0800

    Update dropzone styles

--- res/app.js
+++ res/app.js
@@ -1,13 +1,12 @@
 (function () {
+    Dropzone.autoDiscover = false;
+
     new Dropzone(
         document.getElementById('dropzone'),
         {
+            acceptedFiles: '.zevtc,.evtc.zip,.evtc',
             clickable: true,
             init: function () {
-                this.on('addedfile', function (file) {
-                });
-                this.on('complete', function (file) {
-                });
                 this.on('sending', function (file, xhr, form) {
                     var xhrsend = xhr.send;
                     xhr.send = function () {
@@ -15,14 +14,6 @@
                     };
                     xhr.setRequestHeader('X-ACCESS-TOKEN', document.querySelector('[name="token"]').value);
                     xhr.setRequestHeader('X-EVTC-FILENAME', file.name);
-                });
-                this.on('previewTemplate', function (file) {
-                });
-                this.on('success', function (file) {
-                });
-                this.on('thumbnail', function (file, dataUrl) {
-                });
-                this.on('uploadprogress', function (file, progress, bytesSent) {
                 });
             },
             method: 'put',
